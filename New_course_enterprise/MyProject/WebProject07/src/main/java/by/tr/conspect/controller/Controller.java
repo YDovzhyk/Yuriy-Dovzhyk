c767a70346836571e1f3a91a2c4b89292f310e88
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2021-06-23 20:20:11 +0300

    First Commit
    Our Commit

---
+++ New_course_enterprise/MyProject/WebProject07/src/main/java/by/tr/conspect/controller/Controller.java
@@ -0,0 +1,42 @@
+package by.tr.conspect.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Controller() {
+		super();
+	}
+	
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	private void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		requestDispatcher.forward(request, response);
+
+	}
+
+}
