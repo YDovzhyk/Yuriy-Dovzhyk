9c019a81ee59f64d4d24f1b47c43265cc478e6d6
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2021-06-24 19:10:07 +0300

    First Commit

---
+++ firstwebproject/src/main/java/by/http/newtest/controller/Controller.java
@@ -0,0 +1,42 @@
+package by.http.newtest.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Controller() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, JD2");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>This is new page</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ firstwebproject/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Hello</title>
+</head>
+<body>
+<h1>Hello, JD2-76-21</h1>
+<a href="newurl">go to Controller</a>
+</body>
+</html>
